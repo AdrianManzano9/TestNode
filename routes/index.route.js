e8556cdc2016ebc67559431c99a92941f748c16d
AdrianManzano9 <74309654+AdrianManzano9@users.noreply.github.com>
2022-11-30 12:11:55 -0300

    Add files via upload

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,10 +1,10 @@
 const recursoRouter= require("./recurso.route");
-const UserRouter= require("./user.route");
+const userRouter= require("./user.route");
 
 const express = require("express");
 const indexRoutes = express.Router();
 
-indexRoutes.use('/product',recursoRouter);
-indexRoutes.use('/user',UserRouter);
+indexRoutes.use('/recurso',recursoRouter);
+indexRoutes.use('/user',userRouter);
 
 module.exports =indexRoutes;
